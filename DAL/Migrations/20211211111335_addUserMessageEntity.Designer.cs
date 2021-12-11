2069a54d1f97a439b3db48c6b71e0f8a576e4e2d
Michał Wojtaszek <michal.wojtaszek@microsoft.wsei.edu.pl>
2021-12-11 12:14:33 +0100

    add migration usermessage

--- DAL/Migrations/20211211111335_addUserMessageEntity.Designer.cs
+++ DAL/Migrations/20211211111335_addUserMessageEntity.Designer.cs
@@ -4,14 +4,16 @@ using DAL.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211211111335_addUserMessageEntity")]
+    partial class addUserMessageEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
