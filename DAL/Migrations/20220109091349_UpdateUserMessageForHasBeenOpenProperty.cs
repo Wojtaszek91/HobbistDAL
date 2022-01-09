281afad01816acccf6176546b54e3f9f93d7a68f
Michał Wojtaszek <michal.wojtaszek@microsoft.wsei.edu.pl>
2022-01-09 10:14:34 +0100

    add update user message hasbeenopen property

---
+++ DAL/Migrations/20220109091349_UpdateUserMessageForHasBeenOpenProperty.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class UpdateUserMessageForHasBeenOpenProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RecivedTime",
+                table: "UserMessages");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "HasBeenOpen",
+                table: "UserMessages",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasBeenOpen",
+                table: "UserMessages");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RecivedTime",
+                table: "UserMessages",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
