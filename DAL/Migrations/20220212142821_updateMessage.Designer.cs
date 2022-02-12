3180e78bbd2852c3c6c3311b744d48cfc887ec8d
Michał Wojtaszek <michal.wojtaszek@microsoft.wsei.edu.pl>
2022-02-12 16:06:40 +0100

    add migration

--- DAL/Migrations/20220212142821_updateMessage.Designer.cs
+++ DAL/Migrations/20220212142821_updateMessage.Designer.cs
@@ -4,14 +4,16 @@ using DAL.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220212142821_updateMessage")]
+    partial class updateMessage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DAL/Migrations/20220212142821_updateMessage.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class updateMessage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasBeenSend",
+                table: "UserMessages",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TargetUserName",
+                table: "UserMessages",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasBeenSend",
+                table: "UserMessages");
+
+            migrationBuilder.DropColumn(
+                name: "TargetUserName",
+                table: "UserMessages");
+        }
+    }
+}
